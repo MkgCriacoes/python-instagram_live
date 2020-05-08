3fd5899ad6bc70c901372cb24953fb1896d84012
Markus Luan <markus23_luan@hotmail.com>
2020-05-08 13:05:30 -0300

    Preparando servidor web

--- instagram/stream.py
+++ instagram/stream.py
@@ -3,9 +3,9 @@ import requests
 class Stream():
     def __init__(self, session):
         self.__session = session
-        self.__live_id = None
-        self.__stream_url = None
-        self.__stream_key = None
+        self.id = None
+        self.url = None
+        self.key = None
 
         print("Obtendo chave do Stream")
 
@@ -17,30 +17,30 @@ class Stream():
         })
         res = req.json()
 
-        self.__live_id = str(res["broadcast_id"])
-        self.__stream_url = "rtmps://live-upload.instagram.com:443/rtmp/"
-        self.__stream_key = res["upload_url"].replace(self.__stream_url, "")
+        self.id = str(res["broadcast_id"])
+        self.url = "rtmps://live-upload.instagram.com:443/rtmp/"
+        self.key = res["upload_url"].replace(self.url, "")
 
         print("URL do Stream")
-        print(self.__stream_url)
+        print(self.url)
         print("")
         print("")
 
         print("Chave do Stream")
-        print(self.__stream_key)
+        print(self.key)
         print("")
         print("")
 
     def iniciar(self):
         print("Iniciando stream")
         #_csrftoken=KHYH0aAV1MAuodATGwQL6pSiViT79Im4&_uuid=c2e407d1-0c08-40a4-afd4-a90e00ec6251
-        req = self.__session.post("https://i.instagram.com/api/v1/live/" + self.__live_id+ "/start/")
+        req = self.__session.post("https://i.instagram.com/api/v1/live/" + self.id+ "/start/")
         print(req.json())
         print()
 
     def encerrar(self):
         print("Encerrando stream")
         #signed_body=d8495776e76a44abd12d7c6b753ebb7bbcf8b0fddb30b888fb6a8377a0404d21.{"_csrftoken":"KHYH0aAV1MAuodATGwQL6pSiViT79Im4","_uid":"32566605591","_uuid":"c2e407d1-0c08-40a4-afd4-a90e00ec6251","end_after_copyright_warning":"false"}&ig_sig_key_version=4
-        req = self.__session.post("https://i.instagram.com/api/v1/live/" + self.__live_id+ "/end_broadcast/")
+        req = self.__session.post("https://i.instagram.com/api/v1/live/" + self.id+ "/end_broadcast/")
         print(req.json())
         print()
--- main.py
+++ main.py
@@ -1,11 +1,72 @@
 import instagram
 import time
+import flask
+from flask import Flask, render_template
 
-instagram.fazerLogin("USUARIO", "SENHA")
-stream = instagram.getStream()
+app = Flask(__name__)
 
-time.sleep(2)
-stream.iniciar()
+comentarios = []
+stream = None
+logado = False
 
-time.sleep(2)
-stream.encerrar()
+@app.route("/login", methods=["POST"])
+def fazerLogin():
+    try:
+        instagram.fazerLogin("USUARIO", "SENHA")
+        return "Logado com sucesso!"
+    except:
+        return "Login invalido!"
+
+@app.route("/stream/criar")
+def criarStream():
+    global stream
+    stream = instagram.getStream()
+
+    return {
+        "live_id" : stream.id,
+        "stream_url" : stream.url,
+        "stream_key" : stream.key
+    }
+
+@app.route("/stream/iniciar")
+def iniciarStream():
+    if stream is None:
+        return "Erro: Você deve criar o stream antes de iniciar"
+    
+    stream.iniciar()
+    return "Stream Iniciado"
+
+@app.route("/stream/encerrar")
+def encerrarStream():
+    if stream is None:
+        return "Erro: Não existe streams para encerrar"
+
+    stream.encerrar()
+    stream = None
+    return "Stream Encerrado"
+
+@app.route("/responder/<id>", methods=["POST"])
+def responder(id):
+    #instagram.responderComentario()
+    pass
+
+@app.route("/comentarios")
+def getComentarios():
+    global comentarios
+    
+    novosComentarios = [] #instagram.getComentarios()
+    if comentarios == novosComentarios:
+        time.sleep(0.5)
+        return comentarios
+    
+    comentarios = novosComentarios
+    print("Novo comentário")
+
+    return comentarios
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(port=80, debug=True)
