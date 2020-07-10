975b248cf9f507f57ec65ed2df55b17d53221c9b
Markus Luan <markus23_luan@hotmail.com>
2020-07-09 21:08:58 -0300

    Preparando para exibir video e reproduzir audio

--- instagram/midia.py
+++ instagram/midia.py
@@ -1,4 +1,5 @@
 from .constants import Constants
+import os
 
 class Midia:
     def __init__(self, getSession):
@@ -17,3 +18,35 @@ class Midia:
         res = url + "\n"
         res += req.text
         return res
+
+    def __lerMidia(self, url):
+        url = "https://instagram.fnat1-1.fna.fbcdn.net/hvideo-" + url
+        req = self.__session.get(url, stream=True, headers={
+            'Accept': '*/*'
+        })
+        req.raise_for_status()
+        return req.content
+
+    def midia(self, url, stream_id):
+        self.__session = self.__getSession()
+
+        initFile = ".midia_" + stream_id + "-init." + url[-3:]
+        content = bytes()
+
+        # Se o arquivo solicitado não for o init, retorne os bytes
+        # Se o arquivo solicitado for o init e não tiver sido baixado, faça o download e retorne os bytes
+        if not url[-8:].startswith("init"):
+            content = self.__lerMidia(url)
+        elif not os.path.isfile(initFile):
+            content = self.__lerMidia(url)
+
+            f = open(initFile, "wb")
+            f.write(content)
+            f.close()
+            return content
+        else: # Se o arquivo solicitado for o init e tiver sido baixado, leia e retorne os bytes
+            f = open(initFile, "rb")
+            content = f.read()
+            f.close()
+
+        return content 
--- midia.py
+++ midia.py
@@ -1,75 +1,113 @@
 import instagram
-from flask import request, redirect, render_template
+from flask import request, redirect, render_template, make_response
 import xmltodict
 
 class Midia:
     def __init__(self, app, stream):
         self.__stream = stream
         self.rotas(app)
+
+    def __info(self):
+        stream = self.__stream.value
+
+        #Recebendo informações dos videos e audios e a url principal
+        videoInfo = instagram.Midia.info(stream.id)
+
+        #Separando url
+        url = videoInfo[:videoInfo.index("\n")]
+        videoInfo = videoInfo.replace(url + "\n", "")
+
+        #Tratando mpd
+        url = url[:url.rindex("/")]
+        url = url[:url.rindex("/")+1]
+        url = url.replace("https://instagram.fnat1-1.fna.fbcdn.net/hvideo-", "")
+        videoInfo = videoInfo.replace("../", url)
+
+        videoInfo = xmltodict.parse(videoInfo)
+
+        root = videoInfo["MPD"]["Period"]["AdaptationSet"]
+        v = root[0]["Representation"]
+        a = root[1]["Representation"]
+
+        v_type = v["@mimeType"]
+        a_type = a["@mimeType"]
+
+        v = v["SegmentTemplate"]
+        a = a["SegmentTemplate"]
+
+        v_url = v["@media"].replace("../", "")
+        a_url = a["@media"].replace("../", "")
+
+        v = v["SegmentTimeline"]["S"]
+        a = a["SegmentTimeline"]["S"]
+
+        videos = [{
+            "duration": 0,
+            "url": v_url.replace("$Time$", "init"),
+            "mime_type": v_type
+        }]
+
+        audios = [{
+            "duration": 0,
+            "url": a_url.replace("$Time$", "init"),
+            "mime_type": v_type
+        }]
+
+        for _v in v:
+            videos.append({
+                "duration": _v["@d"],
+                "url": v_url.replace("$Time$", _v["@t"]),
+                "mime_type": v_type
+            })
+
+        for _a in a:
+            audios.append({
+                "duration": _a["@d"],
+                "url": a_url.replace("$Time$", _a["@t"]),
+                "mime_type": v_type
+            })
+
+        midia = {
+            "videos": videos,
+            "audios": audios
+        }
+
+        return midia
+
+    def __midia(self, url):
+        stream = self.__stream.value
+        return instagram.Midia.midia(url, stream.id)
     
     def rotas(self, app):
-        @app.route("/midia/info")
-        def __info():
-            stream = self.__stream.value
+        @app.route("/midia/video")
+        def getVideo():
+            midia = self.__info()
+            content = bytes()
 
-            #Recebendo informações dos videos e audios e a url principal
-            videoInfo = instagram.Midia.info(stream.id)
+            for v in midia["videos"]:
+                content += self.__midia(v["url"])
 
-            url = videoInfo[:videoInfo.index("\n")]
-            videoInfo = videoInfo.replace(url + "\n", "")
+            return make_response(
+                content,
+                200,
+                {
+                    "content-type": "video/mp4"
+                }
+            )
 
-            url = url[:url.rindex("/")]
-            url = url[:url.rindex("/")+1]
-            url = url.replace("https://instagram.fnat1-1.fna.fbcdn.net/hvideo-", "")
-            videoInfo = videoInfo.replace("../", url)
+        @app.route("/midia/audio")
+        def getAudio():
+            midia = self.__info()
+            content = bytes()
 
-            videoInfo = xmltodict.parse(videoInfo)
+            for a in midia["audios"]:
+                content += self.__midia(a["url"])
 
-            root = videoInfo["MPD"]["Period"]["AdaptationSet"]
-            v = root[0]["Representation"]
-            a = root[1]["Representation"]
-
-            v_type = v["@mimeType"]
-            a_type = a["@mimeType"]
-
-            v = v["SegmentTemplate"]
-            a = a["SegmentTemplate"]
-
-            v_url = v["@media"].replace("../", "")
-            a_url = a["@media"].replace("../", "")
-
-            v = v["SegmentTimeline"]["S"]
-            a = a["SegmentTimeline"]["S"]
-
-            videos = [{
-                "duration": 0,
-                "url": v_url.replace("$Time$", "init"),
-                "mime_type": v_type
-            }]
-
-            audios = [{
-                "duration": 0,
-                "url": a_url.replace("$Time$", "init"),
-                "mime_type": v_type
-            }]
-
-            for _v in v:
-                videos.append({
-                    "duration": _v["@d"],
-                    "url": v_url.replace("$Time$", _v["@t"]),
-                    "mime_type": v_type
-                })
-
-            for _a in a:
-                audios.append({
-                    "duration": _a["@d"],
-                    "url": a_url.replace("$Time$", _a["@t"]),
-                    "mime_type": v_type
-                })
-
-            media = {
-                "videos": videos,
-                "audios": audios
-            }
-
-            return media
+            return make_response(
+                content,
+                200,
+                {
+                    "content-type": "video/mp4"
+                }
+            )
+        
